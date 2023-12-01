75004fd3eaae819b7ece57b1fdf3ff3fba4eb6d0
mattkellyirl <matthew.kelly@digicom.net.au>
2023-12-01 14:16:46 +1030

    added javascript

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+/*
+var topic = "HTML";
+
+if (topic === 'HTML') {
+    console.log("Let's Study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's Study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's Study Git!"); 
+} else if (topic === 'Javascript') {
+    console.log("Let's Study Javascript!");
+} else {
+    console.log("Please try again");
+}
+*/
+
+var topics = ["HTML", "CSS", "Git", "Javascript"];
+
+for(var forLoop = 0; forLoop < topics.length; forLoop++) {
+    console.log(topics[forLoop]);
+}
