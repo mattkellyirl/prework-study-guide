ac0e00434fe3d4228d2941e0468b96255601996d
mattkellyirl <matthew.kelly@digicom.net.au>
2023-12-01 17:35:54 +1030

    added javascript functions

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,30 @@
-/*
-var topic = "HTML";
-
-if (topic === 'HTML') {
-    console.log("Let's Study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's Study CSS!");
-} else if (topic === 'Git') {
-    console.log("Let's Study Git!"); 
-} else if (topic === 'Javascript') {
-    console.log("Let's Study Javascript!");
-} else {
-    console.log("Please try again");
-}
-*/
-
 var topics = ["HTML", "CSS", "Git", "Javascript"];
 
-for(var forLoop = 0; forLoop < topics.length; forLoop++) {
-    console.log(topics[forLoop]);
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
+
+function selectTopic () {
+    if (randomTopic === 'HTML') {
+        console.log("Let's Study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's Study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's Study Git!"); 
+    } else if (randomTopic === 'Javascript') {
+        console.log("Let's Study Javascript!");
+    } else {
+        console.log("Please try again");
+    }
+}
+
+console.log("Here are the topics we learned through the prework:");
+listTopics()
+
+console.log("Which topic should we study first?");
+selectTopic ()
+
